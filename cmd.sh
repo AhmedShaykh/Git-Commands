7f2101f3a1b34ab54f2e52dc73b6c3c787128966
Ahmed Saleem Shaikh <98047091+AhmedShaykh@users.noreply.github.com>
2024-05-11 19:18:02 +0500

    Final Commit

--- cmd.sh
+++ cmd.sh
@@ -1,25 +1,25 @@
-git init
-git clone [url]
-git add [file]
-git add .
-git add --all
-git commit -m "message"
-git status
-git log
-git branch
-git branch [branch_name]
-git checkout [branch_name]
-git checkout -b [branch_name]
-git merge [branch_name]
-git pull
-git push
-git remote -v
-git remote add [name] [url]
-git fetch [remote]
-git diff
-git diff [commit1] [commit2]
-git reset [file]
-git reset --hard [commit]
-git tag [tag_name]
-git tag -a [tag_name] -m "message"
-git push --tags
+$ git init
+$ git clone [url]
+$ git add [file]
+$ git add .
+$ git add --all
+$ git commit -m "message"
+$ git status
+$ git log
+$ git branch
+$ git branch [branch_name]
+$ git checkout [branch_name]
+$ git checkout -b [branch_name]
+$ git merge [branch_name]
+$ git pull
+$ git push
+$ git remote -v
+$ git remote add [name] [url]
+$ git fetch [remote]
+$ git diff
+$ git diff [commit1] [commit2]
+$ git reset [file]
+$ git reset --hard [commit]
+$ git tag [tag_name]
+$ git tag -a [tag_name] -m "message"
+$ git push --tags
